de6c30b596692fb56070054ce6e8aef652555a84
Guillaume <guillaume.debeaudoin@gmail.com>
2023-02-06 10:40:50 +0100

    formulaire 80%

--- views/home.php
+++ views/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,40 +17,49 @@
 
     <title>Document</title>
 </head>
+
 <body>
 
-<!-- header -->
-<header class="backGroundOmnias">
+    <!-- header -->
+    <header>
 
-<form class="container col-5" style="background-color: white;">
-    <div >
-        <img class="logoOmniasConnexion" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
-    </div>
-  <div class="mb-3 text-center">
-    <label for="exampleInputEmail1" class="form-label pb-3">Adresse Mail</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3 mt-2 text-center">
-    <label for="exampleInputPassword1" class="form-label pb-3">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="text-end mt-5">
-    <button type="submit" class="btn btn-primary">Valider</button>
-  </div>
-  <div class="text-center mt-5">
-    <a href="">Mot de passe oublié</a>
-  </div>
-  
-</form>
-</header>
-   <!-- footer -->  
-<footer>
+        <form class="container col-5 mt-5" style="background-color: white;">
+            <div>
+                <img class="logoOmniasConnexion mt-3" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
+            </div>
+            <div class="mb-3 text-center shadow-sm mt-3">
+                <label for="exampleInputEmail1" class="form-label pb-3">Adresse Mail</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3 mt-2 text-center shadow-sm">
+                <label for="exampleInputPassword1" class="form-label pb-3">Mot de passe</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="text-end mt-5 ">
+                <button type="submit" class="btn btn-outline-secondary">Valider</button>
+            </div>
+            <div class="text-center mt-5 pb-5">
+                <a style="color: black;" href="">Mot de passe oublié</a>
+            </div>
 
-</footer>
+        </form>
+    </header>
+    <!-- footer -->
+    <footer class="position-absolute bottom-0 start-50 translate-middle-x mb-3">
+        <div class="row">
+            <div class="col-auto">
+                <a style="color: black;" href="">Nous contacter</a>
+            </div>
+            <div class="col">
+                <p>Omnias 2023</p>
+            </div>
+        </div>
+    </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
 
 
 </body>
+
 </html>
