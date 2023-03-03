87b3e43c7d94e6eada3fc6cd4ca88f2c10297777
Guillaume <guillaume.debeaudoin@gmail.com>
2023-03-03 11:20:29 +0100

    putain de bouton bis je dois les positionner et gerer les tailles

--- views/home.php
+++ views/home.php
@@ -26,19 +26,20 @@
         <div class="container col-8 mt-5 borderFormConnexion" style="background-color: white;">
             <div>
                 <img class="logoOmniasConnexion mt-3" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
+                <a class="justify-content-end m-2 col-lg-2 btn btn-outline-secondary fs-3" href="">Mon compte</a>
             </div>
             <div class="container col-6">
 
-                
-                    <div class="row text-center g-4">
-                        <div >
-                            <a class="myButton m-2 col-lg-4 btn btn-outline-secondary fs-3" href="">Pointage</a>
-                            <a class="myButton m-5 col-lg-4 btn btn-outline-secondary fs-3" href="">Note de frais</a>
-                            
-                        </div>
-                        
+
+                <div class="row text-center g-4">
+                    <div>
+                        <a class="myButton m-2 col-lg-4 btn btn-outline-secondary fs-3" href="">Pointage</a>
+                        <a class="myButton m-5 col-lg-4 btn btn-outline-secondary fs-3" href="">Note de frais</a>
+
                     </div>
-               
+
+                </div>
+
 
                 <div class="text-center mt-5 pb-5">
                     <a style="color: black; text-decoration: none;" class="btn btn-outline-danger" href="">Deconnexion</a>
