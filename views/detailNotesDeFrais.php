ce8083af2f3830179021636b09e3cc902994fe3d
Guillaume <guillaume.debeaudoin@gmail.com>
2023-04-07 20:57:20 +0200

    modif front detail note de frais

--- views/detailNotesDeFrais.php
+++ views/detailNotesDeFrais.php
@@ -25,46 +25,55 @@
 
         <div class="container col-8 mt-5 borderFormConnexion" style="background-color: white;">
             <div>
-            <a  href="home.php">
-                <img class="logoOmniasConnexion mt-3" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
+                <a href="home.php">
+                    <img class="logoOmniasConnexion mt-3" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
                 </a>
                 <a class="justify-content-end m-2 col-lg-7 btn btn-outline-secondary fs-3" href="">Mes notes de frais</a>
                 <a class="justify-content-end m-2 col-lg-2 btn btn-outline-secondary fs-3" href="">Mon compte</a>
             </div>
-            <div class="container ">
 
+            <div class="mt-5">
 
-                <div class="row text-center g-4">
+                <div class="">
+                    <ul class="">
+                        <li class="card w-80">
+                            <div class="card-body justify-content-between row">
+                                <p class="fs-4 titleCard btn btn-outline-secondary col">nom de la note de frais</p>
+                                <p class="col btn btn-outline-secondary">date</p>
+                                <p class="truncate col btn btn-outline-secondary">montant</p>
+                                <p class="truncate col btn btn-outline-secondary">en traitement / validé</p>
+                                <p class="truncate btn btn-outline-secondary col">modifiable / non modif</p>
+                                <a href="" class="col btn btn-outline-secondary">Plus d'info</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
 
-                    <div class="row justify-content-center g-0">
-                        <article class="card mb-3 mt-5 mywidthCard ps-lg-3 pt-3 pb-3">
-                            <div class="row g-0 justify-content-center">
-
-                                
+                <div class="container ">
+                    <div class="row text-center g-4">
+                        <div class="row justify-content-center g-0">
+                            <article class="card mb-3 mt-5 mywidthCard ps-lg-3 pt-3 pb-3">
+                                <div class="row g-0 justify-content-center">
                                     <div class="row justify-content-center">
                                         <h3 class="fs-4 titleCard btn btn-outline-secondary col m-1">nom de la note de frais</h3>
                                         <p class="col btn btn-outline-secondary m-1">date</p>
                                         <p class="truncate mb-3 col btn btn-outline-secondary m-1">montant</p>
                                         <p class="truncate mb-3 col btn btn-outline-secondary m-1">en traitement / validé</p>
-                                        
                                         <p class="truncate mb-3 btn btn-outline-secondary col m-1">modifiable / non modif</p>
-                                
                                         <a href="" class="btn blueButton col btn-outline-secondary m-1">Plus d'info</a>
                                     </div>
-                                
-                            </div>
-                        </article>
-
+                                </div>
+                            </article>
+                        </div>
                     </div>
 
+
+                    <div class="text-center mt-5 pb-5">
+                        <a style="color: black; text-decoration: none;" class="btn btn-outline-danger" href="">Deconnexion</a>
+                    </div>
                 </div>
 
-
-                <div class="text-center mt-5 pb-5">
-                    <a style="color: black; text-decoration: none;" class="btn btn-outline-danger" href="">Deconnexion</a>
-                </div>
             </div>
-        </div>
     </header>
     <!-- footer -->
     <footer class="position-absolute bottom-0 start-50 translate-middle-x mb-3">
