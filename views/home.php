a9bd49825827350dd74829cdd8656ea893272a77
Guillaume <guillaume.debeaudoin@gmail.com>
2023-04-11 09:46:12 +0200

    modif front

--- views/home.php
+++ views/home.php
@@ -28,18 +28,20 @@
                 <a href="home.php">
                     <img class="logoOmniasConnexion mt-3" src="../assets/img/OmniasLogo.png" alt="Logo Entreprise Omnias">
                 </a>
+               
                 <a class="justify-content-end m-2 col-lg-2 btn btn-outline-secondary fs-3" href="">Mon compte</a>
 
-                <a class="justify-content-end m-2 col-lg-2 btn btn-outline-secondary fs-3" href="rhAjoutCollaborateur.php">Ajout collaborateur</a>
+                <a class="justify-content-end m-2 col-lg-2 btn btn-outline-secondary fs-4" href="rhAjoutCollaborateur.php">Ajout collaborateur</a>
             </div>
-            <div class="container col-6">
+            <div class="container mt-5">
 
 
                 <div class="row text-center g-4">
-                    <div>
-                        <a class="myButton m-2 col-lg-4 btn btn-outline-secondary fs-3" href="">Pointage</a>
-                        <a class="myButton m-5 col-lg-4 btn btn-outline-secondary fs-3" href="menuNotesdeFrais.php">Note de frais</a>
-
+                    <div class="col">
+                        <a class="myButton  btn btn-outline-secondary fs-3" href="">Pointage</a>
+                    </div>
+                    <div class="col">
+                        <a class="myButton  btn btn-outline-secondary fs-3" href="menuNotesdeFrais.php">Note de frais</a>
                     </div>
 
                 </div>
